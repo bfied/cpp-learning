74c699ce5621d7e7562c11bd0a89d385c8eb7a2b
bfied <bfiedler313@gmail.com>
2023-12-28 15:32:58 -0800

    updates

--- Section7/Arrays/main.cpp
+++ Section7/Arrays/main.cpp
@@ -11,17 +11,20 @@ int main() {
     cout << "\nThe first vowel is: " << vowels[0] << endl;
     cout << "The last vowel is: " << vowels[4] << endl;
     
-    cin >> vowels[4]; // out of bounds - don't do this!!
-    
+ //   cin >> vowels[5]; // out of bounds - don't do this!!
+ //   cout << vowels;
+
+
     double hi_temps []  { 90.1, 89.8, 77.5, 81.6};
-    cout << "\nThe first high temperature is: " << hi_temps[0] << endl;
+    int last_element = size(hi_temps)-1;
+    cout << "\nThe first high temperature is: " << hi_temps[last_element] << endl;
     
     hi_temps[0] = 100.7;    // set the first element in hi_temps to 100.7
                                        // we'll talk about assignment statement in an upcoming section
-    
+
     cout << "The first high temperature is now: " << hi_temps[0] << endl;    
   
-    int test_scores [] {100, 90, 80,70, 60};
+    int test_scores [5] {};
   
     cout << "\nFirst score at index 0: " << test_scores[0] << endl;
     cout << "Second score at index 1: " << test_scores[1] << endl;
@@ -29,12 +32,12 @@ int main() {
     cout << "Fourth score at index 3: " << test_scores[3] << endl;
     cout << "Fifth score at index 4: " << test_scores[4] << endl;
     
-    cout << "\nEnter 5 test scores: ";
-    cin >> test_scores[0];
-    cin >> test_scores[1];
-    cin >> test_scores[2];
-    cin >> test_scores[3];
-    cin >> test_scores[4];
+    // cout << "\nEnter 5 test scores: ";
+    // cin >> test_scores[0];
+    // cin >> test_scores[1];
+    // cin >> test_scores[2];
+    // cin >> test_scores[3];
+    // cin >> test_scores[4];
     
     cout << "\nThe updated array is:" << endl;
     cout << "First score at index 0: " << test_scores[0] << endl;
@@ -45,8 +48,24 @@ int main() {
    
 
     cout << "\nNotice what the value of the array name is : " << test_scores << endl;
-
+    cout <<"size of array: " << size(test_scores);
     cout << endl;
+
+
+    // multi dimensional array initialization
+    int reviewers {3};
+    int movies {4};
+    int movie_rating[reviewers][movies]{
+
+        {0,5,2,4},
+        {2,4,5,6},
+        {1,4,5,1}
+        };
+
+    cout << "movie rating reviewer [2][1], should be 4:  " << movie_rating[2][1] << endl;
+
+
     return 0;
 }
 
+;
--- Section7/Challenge/main.cpp
+++ Section7/Challenge/main.cpp
@@ -30,12 +30,36 @@
 */
     
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
     
     // your solution goes here
+    vector <int> vector1{};
+    vector <int> vector2{};
+    vector1.push_back(10);
+    vector1.push_back(20);
+
+    cout << "vector1 first and second elements and size: " << vector1.at(0) << " " << vector1.at(1) << " " << size(vector1) << endl;
+
+    vector2.push_back(100);
+    vector2.push_back(200);
+
+    vector < vector<int> > vector2d {};
+
+    vector2d.push_back(vector1);
+
+    vector2d.push_back(vector2);
+
+    vector1[0] = 1000;
+
+    cout << "the modified vector is: " << vector2d[0][0];
 
     cout << endl;
+
+
     return 0;
 }
 
